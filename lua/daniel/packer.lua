7d31d66adb60578906d2494f9fcceeeb53b5e52f
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2023-09-09 13:54:51 +0200

    (feat) Adds yaml and autopairs plugins

--- lua/daniel/packer.lua
+++ lua/daniel/packer.lua
@@ -40,4 +40,20 @@ use{
 		{'rafamadriz/friendly-snippets'},
 	}
 }
+use {
+	"someone-stole-my-name/yaml-companion.nvim",
+	requires = {
+		{ "neovim/nvim-lspconfig" },
+		{ "nvim-lua/plenary.nvim" },
+		{ "nvim-telescope/telescope.nvim" },
+	},
+	config = function()
+		require("telescope").load_extension("yaml_schema")
+	end,
+}
+use {
+	"windwp/nvim-autopairs",
+	config = function() require("nvim-autopairs").setup {}
+	end
+}
 end)
