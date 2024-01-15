cf7bdf910b12c3ecb2614012ce01a821953f0a6e
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2024-01-16 00:27:44 +0100

    (feat) Starter config for kanagawa style

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -1,9 +1,39 @@
-function ColorMyPencils(color)
-	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
+-- in real config it should only be one color config
+function ColorMyPencils()
+	require("kanagawa").setup({
+		transparent=true,
+		undercurl=true,
+		terminalColors=true,
+		commentStyle= { italic=true },
+		background = {
+			dark = "dragon"
+		},
+		overrides = function(colors)
+			local theme = colors.theme
+			return {
+				NormalFloat = {bg = "none"},
+				FloatBorder = {bg = "none"},
+				FloatTitle = {bg = "none"},
+				NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+				LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+				MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+				String = {fg = colors.palette.carpYellow, italic = false }
+			}
+		end,
+		colors = {
+			theme = {
+				all = {
+					ui = {
+						bg_gutter = "none"
+					}
+				}
+			}
+		}
+	})
+	vim.cmd.colorscheme("kanagawa")
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+--	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 ColorMyPencils()
--- lua/daniel/packer.lua
+++ lua/daniel/packer.lua
@@ -11,13 +11,7 @@ return require('packer').startup(function(use)
 	  'nvim-telescope/telescope.nvim', tag = '0.1.5',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
-  use({
-	  'rose-pine/neovim',
-	  as = 'rose-pine',
-	  config = function()
-		  vim.cmd('colorscheme rose-pine')
-	  end
-  })
+use('rebelot/kanagawa.nvim')
 use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 use('nvim-treesitter/playground')
 use('theprimeagen/harpoon')
