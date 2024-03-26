6c1163c4703a4d89fbaa52f9e1a5535dcfe71378
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2024-03-26 23:35:18 +0100

    (feat) Refactors the lsp config without lsp-zero

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -1,23 +1,23 @@
--- in real config it should only be one color config
 function ColorMyPencils()
 	require("kanagawa").setup({
-		transparent=true,
-		undercurl=true,
-		terminalColors=true,
-		commentStyle= { italic=true },
+		transparent = true,
+		undercurl = true,
+		terminalColors = true,
+		commentStyle = { italic = true },
+		keywordStyle = { bold = true },
 		background = {
 			dark = "dragon"
 		},
 		overrides = function(colors)
 			local theme = colors.theme
 			return {
-				NormalFloat = {bg = "none"},
-				FloatBorder = {bg = "none"},
-				FloatTitle = {bg = "none"},
+				NormalFloat = { bg = "none" },
+				FloatBorder = { bg = "none" },
+				FloatTitle = { bg = "none" },
 				NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
 				LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
 				MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-				String = {fg = colors.palette.carpYellow, italic = false }
+				String = { fg = colors.palette.carpYellow, italic = false }
 			}
 		end,
 		colors = {
@@ -33,7 +33,7 @@ function ColorMyPencils()
 	vim.cmd.colorscheme("kanagawa")
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
---	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	--	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 ColorMyPencils()
--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,87 +1,106 @@
-local lsp = require('lsp-zero')
+vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
+vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
+vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
 
-lsp.preset('recommended')
+vim.api.nvim_create_autocmd('LspAttach', {
+	desc = 'LSP Actions',
+	callback = function(event)
+		local opts = { buffer = event.buf }
 
-lsp.ensure_installed({
-	'tsserver',
-	'gopls',
-	'pylsp',
-	'lua_ls'
+		-- these will be buffer-local keybindings
+		-- they will only be registered if there's an active language server
+		vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+		vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+		-- note: most servers won't implement declaration (only definition)
+		vim.keymap.set('n', 'gD', function() vim.lsp.buf.declaration() end, opts)
+		vim.keymap.set('n', 'go', function() vim.lsp.buf.type_definition() end, opts)
+		vim.keymap.set('n', 'gi', function() vim.lsp.buf.implementation() end, opts)
+
+		-- might want to use Telescope's references here...
+		vim.keymap.set('n', 'gr', function() vim.lsp.buf.references() end, opts)
+		vim.keymap.set('n', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
+		-- These are default anyway but added for clarity
+		vim.keymap.set('n', '<F2>', function() vim.lsp.buf.rename() end, opts)
+		vim.keymap.set('n', '<F3>', function() vim.lsp.buf.format() end, opts)
+
+		vim.keymap.set('n', 'ga', function() vim.lsp.buf.code_action() end, opts)
+	end
 })
 
-local cmp = require('cmp')
-local cmp_select = { behavior = cmp.SelectBehavior.Select }
-local cmp_mappings = lsp.defaults.cmp_mappings({
-	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-	['<C-y>'] = cmp.mapping.confirm({ select = true }),
-	['<C-space>'] = cmp.mapping.complete(),
-})
+local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-lsp.set_preferences({
-	sign_icons = {}
-})
+local default_setup = function(server)
+	require('lspconfig')[server].setup({
+		capabilities = lsp_capabilities
+	})
+end
 
-
-lsp.setup_nvim_cmp({
-	mapping = cmp_mappings
-})
-
-
-lsp.configure('pylsp', {
-	settings = {
-		pylsp = {
-			plugins = {
-				flake8 = { enabled = false },
-				pycodestyle = { enabled = true, maxLineLength = 120 },
-				pyflakes = { enabled = false },
-				pylint = { enabled = true, args = { "--max-line-length=120", "--disable=C0114,C0115,C0116" } },
-				mccabe = { enabled = false },
-			}
-		}
-	}
-})
 
 require("neodev").setup({
 	-- neovim setup for for init.lua and plugin development with signature_help and docs and completion for the neovim lua API
 	-- setup needs to be called before the lua lsp setup
 })
 
-lsp.configure('lua_ls', {
-	filetypes = { 'lua' },
-	settings = {
-		Lua = {
-			diagnostics = {
-				globals = { 'vim', 'describe', 'it', 'P', 'before_each' }
-			},
-			runtime = {
-				version = "LuaJIT",
-			}
-		}
+require('mason').setup({})
+require('mason-lspconfig').setup({
+	ensure_installed = {},
+	handlers = {
+		default_setup,
+		lua_ls = function()
+			require('lspconfig').lua_ls.setup({
+				capabilities = lsp_capabilities,
+				settings = {
+					Lua = {
+						runtime = {
+							version = 'LuaJIT'
+						},
+						diagnostics = {
+							globals = { 'vim', 'describe', 'before_each', 'after_each', 'it' }
+						},
+						workspace = {
+							library = {
+								vim.env.VIMRUNTIME
+							}
+						}
+					}
+				}
+			})
+		end,
+		pylsp = function()
+			require("lspconfig").pylsp.setup({
+				capabilities = lsp_capabilities,
+				settings = {
+					pylsp = {
+						plugins = {
+							flake8 = { enabled = false },
+							pycodestyle = { enabled = true, maxLineLength = 120 },
+							pyflakes = { enabled = false },
+							pylint = { enabled = true, args = { "--max-line-length=120", "--disable=C0114,C0115,C0116" } },
+							mccabe = { enabled = false },
+						}
+					}
+				}
+			})
+		end
 	}
 })
 
 
-lsp.on_attach(function(client, bufnr)
-	local opts = { buffer = bufnr, remap = false }
+local cmp = require('cmp')
+cmp.setup({
+	sources = {
+		{ name = 'nvim_lsp' },
+	},
+	mapping = cmp.mapping.preset.insert({
+		-- Enter key confirms completion item
+		['<CR>'] = cmp.mapping.confirm({ select = false }),
 
-	vim.keymap.set('n', "gd", function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set('n', "K", function() vim.lsp.buf.hover() end, opts)
-	vim.keymap.set('n', "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-
-	vim.keymap.set('n', "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-	vim.keymap.set('n', "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-
-	vim.keymap.set('n', "[d", function() vim.diagnostic.goto_next() end, opts)
-	vim.keymap.set('n', "]d", function() vim.diagnostic.goto_prev() end, opts)
-
-	vim.keymap.set('n', "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-	vim.keymap.set('n', "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-
-	-- These are default anyway but added for clarity
-	vim.keymap.set('n', "<F2>", function() vim.lsp.buf.rename() end, opts)
-	vim.keymap.set('n', "<F3>", function() vim.lsp.buf.format() end, opts)
-end)
-
-
-lsp.setup()
+		-- Ctrl + space triggers completion menu
+		['<C-Space>'] = cmp.mapping.complete(),
+	}),
+	snippet = {
+		expand = function(args)
+			require('luasnip').lsp_expand(args.body)
+		end,
+	},
+})
