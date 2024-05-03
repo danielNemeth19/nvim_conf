e25dcde74856dd77b66805e53de7b0d41136d76a
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2024-05-04 00:07:23 +0200

    (feat) Rose-pine setup

--- lua/daniel/plugins/color.lua
+++ lua/daniel/plugins/color.lua
@@ -1,63 +1,32 @@
 return {
-    'rebelot/kanagawa.nvim',
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    lazy = false,
     priority = 1000,
-    config = function()
-        require("kanagawa").setup({
-            transparent = true,
-            undercurl = true,
-            terminalColors = true,
-            commentStyle = { italic = true },
-            keywordStyle = { bold = true },
-            background = {
-                dark = "dragon"
+    opts = {
+        styles = {
+            transparency = true,
+            bold = false,
+            italic = false,
+        },
+        highlight_groups = {
+            Keyword = {
+                italic = true,
+                bold = true
             },
-            overrides = function(colors)
-                local theme = colors.theme
-                return {
-                    NormalFloat = { bg = "none" },
-                    FloatBorder = { bg = "none" },
-                    FloatTitle = { bg = "none" },
-                    NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-                    LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-                    MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-                    String = { fg = colors.palette.carpYellow, italic = false }
-                }
-            end,
-            colors = {
-                theme = {
-                    all = {
-                        ui = {
-                            bg_gutter = "none"
-                        }
-                    }
-                }
+            ['@variable.builtin'] = {
+                italic = true
+            },
+            ['@keyword.conditional'] = {
+                italic = true
+            },
+            ['@keyword.repeat'] = {
+                italic = true
             }
-        })
-        vim.cmd.colorscheme("kanagawa")
-
-        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+        }
+    },
+    config = function(_, opts)
+        require('rose-pine').setup(opts)
+        vim.cmd('colorscheme rose-pine')
     end
 }
--- return {
--- "folke/tokyonight.nvim",
--- priority = 1000,
--- init = function ()
--- vim.cmd.colorscheme('tokyonight-night')
--- end
--- }
--- return {
--- 'rose-pine/neovim',
--- name = 'rose-pine',
--- lazy = false,
--- priority = 1000,
--- opts = {
--- styles = {
--- transparency = true,
--- },
--- },
--- config = function(_, opts)
--- require('rose-pine').setup(opts)
--- vim.cmd('colorscheme rose-pine')
--- end
--- }
