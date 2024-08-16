644a7fa411b04d0385152b23adb33b896583835c
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2024-08-17 00:11:27 +0200

    (feat) Adds copilot and copilot-chat config

--- lua/daniel/set.lua
+++ lua/daniel/set.lua
@@ -34,3 +34,7 @@ vim.opt.colorcolumn = "180"
 vim.g.mapleader = " "
 
 vim.opt.showmode = false
+
+vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+vim.api.nvim_set_keymap("n", "<leader>c", '<cmd>CopilotChatToggle <CR>', {})
