19227f90fac7d449d57a53f58048bf2c4ef065ee
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2024-09-15 10:55:18 +0200

    (fix) Cleanes up autocommands setup

--- lua/daniel/plugins/lspconfig.lua
+++ lua/daniel/plugins/lspconfig.lua
@@ -30,17 +30,19 @@ return {
                 vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, opts)
                 vim.keymap.set('n', '<F3>', vim.lsp.buf.format, opts)
 
+                -- The following two autocommands are used to highlight references of the
+                -- word under your cursor when your cursor rests there for a little while.
+                --    See `:help CursorHold` for information about when this is executed
+                --
+                -- When you move your cursor, the highlights will be cleared (the second autocommand).
+
                 local client = vim.lsp.get_client_by_id(event.data.client_id)
                 if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
                     local highlight_augroup = vim.api.nvim_create_augroup('lsp-highlight', { clear = false })
                     vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
                         buffer = event.buf,
                         group = highlight_augroup,
-                        -- callback = vim.lsp.buf.document_highlight,
-                        callback = function()
-                            print('cursor hold executed')
-                            vim.lsp.buf.document_highlight()
-                        end,
+                        callback = vim.lsp.buf.document_highlight,
                     })
 
                     vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
