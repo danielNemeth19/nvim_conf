5a8cf7c5482d2b25a474909108a99fce7470c225
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2025-02-27 18:44:12 +0100

    (feat) Adds quick links to welcome screen

--- lua/daniel/plugins/alpha-nvim.lua
+++ lua/daniel/plugins/alpha-nvim.lua
@@ -1,19 +1,29 @@
 return {
     "goolord/alpha-nvim",
-    dependencies = {'nvim-tree/nvim-web-devicons'},
-    config = function ()
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
         local theta = require("alpha.themes.theta")
+        local dashboard = require("alpha.themes.dashboard")
         theta.file_icons.provider = "devicons"
         theta.header.val = {
-    "                                                     ",
-    "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-    "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-    "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-    "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-    "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-    "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-    "                                                     ",
+            "                                                     ",
+            "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+            "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+            "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+            "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+            "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+            "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+            "                                                     ",
         }
+        theta.buttons.val = {
+            { type = "text",    val = "Quick links", opts = { hl = "SpecialComment", position = "center" } },
+            { type = "padding", val = 1 },
+            dashboard.button("e", "  New file", "<cmd>ene<CR>"),
+            dashboard.button("leader ff", "󰈞  Find file", "<leader>ff"),
+            dashboard.button("leader nc", "  Configuration", "<leader>nc"),
+            dashboard.button("leader ps", "󰈞  Live grep", "<leader>ps"),
+            dashboard.button("q", "󰅚  Quit", "<cmd>qa<CR>"),
+        },
         require("alpha").setup(
             theta.config
         )
