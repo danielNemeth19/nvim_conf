5a905bcc811f60313cb3c8343198654758291d6c
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2026-01-01 18:34:26 +0100

    (chore) Adapts treesitter config to main branch as development on master is locked

--- lua/daniel/plugins/treesitter.lua
+++ lua/daniel/plugins/treesitter.lua
@@ -1,89 +1,97 @@
--- return {
-  -- {
-    -- 'nvim-treesitter/nvim-treesitter',
-    -- build = ':TSUpdate',
-    -- opts = {
-      -- ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "go", "fish", "typescript" },
-      -- auto_install = true,
-      -- sync_install = false,
-      -- highlight = {
-        -- enable = true,
+local log_level = vim.log.levels.INFO
+local branch = 'main'
 
-        -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-        -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-        -- the name of the parser)
-        -- list of language that will be disabled
-        --disable = { "c", "rust" },
+local function notify(msg, level)
+  if level >= log_level then
+    vim.notify(msg, level)
+  end
+end
 
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        -- additional_vim_regex_highlighting = false,
-      -- },
-      -- indent = { enable = true },
-    -- },
-    -- config = function(_, opts)
-      -- require("nvim-treesitter.install").prefer_git = true
-      -- require("nvim-treesitter.configs").setup(opts)
-    -- end
-  -- },
-  -- { "nvim-treesitter/playground" }
--- }
+local function register(ensure_installed)
+  for filetype, parser in pairs(ensure_installed) do
+    local filetypes = vim.treesitter.language.get_filetypes(parser)
+    if not vim.tbl_contains(filetypes, filetype) then
+      table.insert(filetypes, filetype)
+    end
+
+    vim.treesitter.language.register(parser, filetypes)
+  end
+end
+
+local function install_and_start()
+  vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
+    callback = function(event)
+      local bufnr = event.buf
+      local filetype = vim.api.nvim_get_option_value('filetype', { buf = bufnr })
+
+      if filetype == '' then
+        return
+      end
+
+      local parser_name = vim.treesitter.language.get_lang(filetype)
+      notify('parser name is ' .. parser_name, vim.log.levels.DEBUG)
+      if not parser_name then
+        notify('Filetype' .. vim.inspect(filetype) .. ' has no parser registered', vim.log.levels.DEBUG)
+        return
+      end
+
+      local parser_configs = require 'nvim-treesitter.parsers'
+      notify('parser configs: ' .. vim.inspect(parser_configs), vim.log.levels.DEBUG)
+      local parser_can_be_used = nil
+      if branch == 'master' then
+        parser_can_be_used = parser_configs.list[parser_name]
+      elseif branch == 'main' then
+        parser_can_be_used = parser_configs[parser_name]
+        notify('parser can be used' .. vim.inspect(parser_can_be_used), vim.log.levels.DEBUG)
+      end
+      if not parser_can_be_used then
+        return
+      end
+
+      local parser_installed = pcall(vim.treesitter.get_parser, bufnr, parser_name)
+      notify('parser installed: ' .. vim.inspect(parser_installed), vim.log.levels.DEBUG)
+
+      if not parser_installed then
+        if branch == 'master' then
+          vim.cmd('TSInstallSync ' .. parser_name)
+        elseif branch == 'main' then
+          require('nvim-treesitter').install({ parser_name }):wait(30000)
+        end
+      end
+
+      parser_installed = pcall(vim.treesitter.get_parser, bufnr, parser_name)
+      notify('parser installed 2: ' .. vim.inspect(parser_installed), vim.log.levels.DEBUG)
+      if not parser_installed then
+        notify('Failed to get parser for ' .. parser_name .. ' after installation', vim.log.levels.WARN)
+        return
+      end
+
+      vim.treesitter.start(bufnr, parser_name)
+    end,
+  })
+end
 
 return {
   ---@module 'lazy'
   ---@type LazySpec
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
-    event = 'VeryLazy',
-    dependencies = {
-      { 'folke/ts-comments.nvim', opts = {} },
-    },
-
+    event = 'BufRead',
     branch = 'main',
-    build = function()
-      -- update parsers, if TSUpdate exists
-      if vim.fn.exists(':TSUpdate') == 2 then vim.cmd('TSUpdate') end
-    end,
+    build = ':TSUpdate',
 
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     ---@module 'nvim-treesitter'
     ---@type TSConfig
     ---@diagnostic disable-next-line: missing-fields
-    opts = {},
+    opts = {
+      -- Other plugins can pass in desired filetype / parser combos
+      ensure_installed = {},
+    },
 
     config = function(_, opts)
-      local ensure_installed = {
-        'c',
-        'python',
-        'go',
-        'lua',
-        'typescript',
-        'javascript',
-        'html',
-        'css',
-        'fish',
-        'bash',
-        'vim',
-        'vimdoc',
-        'query',
-        'diff',
-        'gitcommit',
-        'luadoc',
-        'markdown',
-        'markdown_inline',
-      }
-      -- ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "go", "fish", "typescript" },
-
-      -- make sure nvim-treesitter can load
-      local ok, nvim_treesitter = pcall(require, 'nvim-treesitter')
-
-      -- no nvim-treesitter, maybe fresh install
-      if not ok then return end
-
-      nvim_treesitter.install(ensure_installed)
+      register(opts.ensure_installed)
+      install_and_start()
     end,
-  },
-  -- { "nvim-treesitter/playground" }
+  }
 }
