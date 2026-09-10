585ddade5d7ec25143489e5b646ea041067539c6
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2026-09-10 23:52:44 +0200

    (feat) Adds lua ftplugin file to set identation - adds autocommand to start treesitter highlighting

--- lua/daniel/autocommands.lua
+++ lua/daniel/autocommands.lua
@@ -5,3 +5,10 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end
 })
+
+-- Enable Treesitter highlighting for all filetypes with a parser
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function ()
+    pcall(vim.treesitter.start)
+  end,
+})
